38be8070d70e88614db9d9bbaea7f5ed66e7c149
devmadan <dev.madaan@nftically.com>
2024-03-08 15:59:59 +0530

    initail

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
+
+function App() {
+
+  
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallenge title="easy" targetTime={1}/>
+        <TimerChallenge title="not easy" targetTime={5}/>
+        <TimerChallenge title="getting hard" targetTime={10}/>
+        <TimerChallenge title="pros only" targetTime={15}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
